35830f6dda1daac0286729db4211f605aea35b4f
Mayuri <mayuri@yopmail.com>
2024-05-02 15:48:08 +0530

    First commit

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+describe StringCalculator do
+  describe 'add' do
+
+  end
+end
---
+++ string_calculator.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# This class accepts a string of numbers as input and returns a sum of those numbers as output.
+class StringCalculator
+  def add
+    # calculator logic will be here
+  end
+end
