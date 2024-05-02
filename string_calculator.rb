8c5277c3ebbf2a1f2d76ad1cac0e434e6249e391
Mayuri <mayuri@yopmail.com>
2024-05-02 17:51:26 +0530

    Refactored code

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,32 +5,43 @@ require 'pry'
 # This class accepts a string of numbers as input and returns a sum of those numbers as output.
 class StringCalculator
   def self.add(numbers)
-    raise ArgumentError, 'Only a string argument is allowed.' unless numbers.is_a?(String)
-
+    validate_input(numbers)
     return 0 if numbers.empty?
 
-    # Default delimiter is ","
-    delimiter = ','
-
-    # Check if custom delimiter is provided
-    if numbers.start_with?('//')
-      delimiter = numbers[2]
-      # Remove custom delimiter declaration from numbers string
-      numbers = numbers.split("\n", 2)[1]
-    end
+    delimiter = get_delimiter(numbers)
+    numbers = remove_delimiter_declaration(numbers)
 
     # Handle invalid input with comma followed by newline
     return 'Invalid input' if numbers.include?(",\n")
 
-    # Replace new lines with delimiter for handling new lines between numbers
-    numbers = numbers.gsub("\n", delimiter)
-    nums = numbers.split(delimiter).map(&:to_i)
+    nums = get_numbers(numbers, delimiter)
+    check_negative_numbers(nums)
+    nums.sum
+  end
 
+  def self.validate_input(numbers)
+    raise ArgumentError, 'Only a string argument is allowed.' unless numbers.is_a?(String)
+  end
+
+  def self.get_delimiter(numbers)
+    # Check if custom delimiter is provided
+    numbers.start_with?('//') ? numbers[2] : ','
+  end
+
+  def self.remove_delimiter_declaration(numbers)
+    # Remove custom delimiter declaration from numbers string
+    numbers.start_with?('//') ? numbers.split("\n", 2)[1] : numbers
+  end
+
+  def self.get_numbers(numbers, delimiter)
+    # Replace new lines with delimiter for handling new lines between numbers
+    numbers.gsub("\n", delimiter).split(delimiter).map(&:to_i)
+  end
+
+  def self.check_negative_numbers(nums)
     # Check for negative numbers
     negatives = nums.select(&:negative?)
     # Raise exception with negative numbers if found
     raise "negative numbers are not allowed, such as #{negatives.join(',')}" if negatives.any?
-
-    nums.sum
   end
 end
